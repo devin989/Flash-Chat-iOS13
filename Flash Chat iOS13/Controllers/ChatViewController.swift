937ac6c4d93b8a4f7e9df4ae28da141593ee59d0
devin989 <deveenrath@gmail.com>
2021-04-24 22:40:55 +0530

    fetching messages from Firestore and sorting messages

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,14 +14,69 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    let db = Firestore.firestore()
+    
+    var messages :[Message]=[
+        Message(sender: "1@abc.com", body: "hello"),
+        Message(sender: "2@abc.com", body: "hey"),
+        Message(sender: "3@abc.com", body: "whats up"),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "⚡️FlashChat"
+        title = K.appName
+        tableView.dataSource = self
         navigationItem.hidesBackButton = true
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil   ), forCellReuseIdentifier: K.cellIdentifier)
+        loadMessages()
+        
+    }
+    
+    func loadMessages(){
+        db.collection(K.FStore.collectionName).order(by: K.FStore.dateField).addSnapshotListener( { (querySnapshot, err) in
+                self.messages = []
 
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    let data = document.data()
+                    if let sender = data[K.FStore.senderField] as? String, let messageBody = data[K.FStore.bodyField] as? String{
+                        let newMessage = Message(sender: sender, body: messageBody)
+                        self.messages.append(newMessage)
+                        
+                        
+                        DispatchQueue.main.async {
+                            self.tableView.reloadData()
+                        }
+                        
+                    }
+                    
+                    //                    print("\(document.documentID) => \(document.data())")
+                }
+            }
+        }
+        )
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        
+        if let messageBody = messageTextfield.text,let messageSender = Auth.auth().currentUser?.email{
+            db.collection(K.FStore.collectionName).addDocument(data: [
+                K.FStore.senderField:messageSender,
+                K.FStore.bodyField:messageBody,
+                K.FStore.dateField:Date().timeIntervalSince1970
+            
+            ]) { (error) in
+                if let e = error{
+                    print(e)
+                }else{
+                    print("Successfully saved data. ")
+                }
+            }
+        }
+        
+        
     }
     
     @IBAction func logOutPressed(_ sender: Any) {
@@ -35,5 +90,22 @@ class ChatViewController: UIViewController {
         }
       
     }
+    
+}
+
+extension ChatViewController:UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        print(messages.count)
+        
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text=messages[indexPath.row].body
+        return cell;
+        
+    }
+    
     
 }
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by DEVEEN RATNAYAKE on 2021-04-24.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender:String
+    let body:String
+}
